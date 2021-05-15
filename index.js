4ec603a779b11498810920dbfcf1dd5872b771e4
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2021-05-15 16:08:50 -0300

    refact

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const puppeteer = require("puppeteer");
 
 // https://hidemy.name/en/proxy-list/?maxtime=100#list
-
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arr = [1, 2, 3, 4, 5];
-// const arr = [1];
+// const arr = [1, 2, 3, 4, 5];
+const arr = [1];
 arr.map(async (value, index, arr) => {
   await bot(value);
 });
 
 async function bot(value) {
   try {
-    const PROXY_HOST = "167.172.171.115";
-    const PROXY_PORT = "40061";
-    const PROXY_TYPE = "https";
+    const PROXY_HOST = "127.0.0.1";
+    const PROXY_PORT = "9050";
+    const PROXY_TYPE = "socks5";
     const PROXY_USERNAME = "username";
     const PROXY_PASSWORD = "password";
 
     const launchOptions = {
+      // headless: false,
       args: [`--proxy-server=${PROXY_TYPE}://${PROXY_HOST}:${PROXY_PORT}`],
     };
 
-    // const browser = await puppeteer.launch(launchOptions);
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(launchOptions);
     console.log("browser opened");
 
     const page = await browser.newPage();
     console.log("page created");
 
-    // await page.authenticate({
-    //   username: PROXY_USERNAME,
-    //   password: PROXY_PASSWORD,
-    // });
-    // console.log("page authenticated");
+    await page.goto("https://check.torproject.org/");
+    const isUsingTor = await page.$eval("body", (el) =>
+      el.innerHTML.includes(
+        "Congratulations. This browser is configured to use Tor"
+      )
+    );
+    if (!isUsingTor) {
+      console.log("Not using Tor. Closing...");
+      await browser.close();
+      return;
+    } else {
+      console.log("Is using Tor. Continuing...");
+    }
 
-    await page.goto("https://youtu.be/O60p7Eb3-tk");
+    await page.goto("https://www.youtube.com/watch?v=6uxlwvwn7lo");
     console.log("page opened");
 
     await page.click("button.ytp-play-button");
     console.log("play clicked");
-    // ytp-time-duration
-    // await page.waitForSelector(".ytp-time-duration");
+
     const min = 60000;
-    const max = 120000;
-    // const max = await page.evaluate(
-    //   () => document.querySelector("span.ytp-time-duration").textContent
-    // );
+    const max = 120000; // todo: calculate the max duration
     const time = random(min, max);
 
-    console.log(`watching ${time} seconds`);
     await delay(time);
     console.log(`watched ${time} seconds`);
 
